b446a89991d8f78c34e938471a2f3cacb452c734
Jimmy Fa <jimmy@andespacificosur.com>
2021-12-04 09:16:43 -0500

    dos

--- Quick2.java
+++ Quick2.java
@@ -1,5 +1,5 @@
 class QuickStart {
     public static void main (String[] args) {
-        System.out.println("Hello, World.Quick");
+        System.out.println("Hello, World.Quick Jimmy");
     }
 }
