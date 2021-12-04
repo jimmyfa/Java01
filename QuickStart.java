04841ebf83bccacbabdd3cc503d55c797be78aed
Jimmy Fa <jimmy@andespacificosur.com>
2021-12-04 09:33:20 -0500

    jimmy

--- QuickStart.java
+++ QuickStart.java
@@ -4,6 +4,6 @@ class QuickStart {
         System.out.println("Hello, World.2");//segundo cambio
         //jimmy
         System.out.println("Hello, World3.");
-        System.out.println("Hello, World3.");//comentario 2
+        System.out.println("Hello, World3.");//comentario 2 3  s
     }
 }
